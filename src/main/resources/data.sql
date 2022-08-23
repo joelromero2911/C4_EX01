5bc6bbefb89fdc1354bdc6588b32a1f515f4d113
joelromero2911 <108723592+joelromero2911@users.noreply.github.com>
2022-08-23 12:45:20 +0200

    data.sql update, added postman collection

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
+DROP table IF EXISTS Usuario;
+
 create table Usuario (
 	id int auto_increment,
     username varchar(255),
@@ -5,4 +7,7 @@ create table Usuario (
     role varchar(255)
 );
 
-INSERT INTO Usuario (username, password, role) VALUES ('admin', 'admin','ADMIN');
+INSERT INTO Usuario (username, password, role) VALUES ('joel', '123','ADMIN');
+INSERT INTO Usuario (username, password, role) VALUES ('jose', 'root','USER');
+INSERT INTO Usuario (username, password, role) VALUES ('a', '123','USER');
+INSERT INTO Usuario (username, password, role) VALUES ('b', '123','USER');
